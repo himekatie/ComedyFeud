3e2c7e16e9afd3199970cd43255be36268546ad9
himekatie <useforanything9@gmail.com>
2025-01-27 19:23:17 -0500

    Changed the score to appear on the sides when the middle score is pressed

--- src/Board/Board.jsx
+++ src/Board/Board.jsx
@@ -8,16 +8,27 @@ import Score from '../Score/Score';
 const Board = () => {
     const [sharedData, setSharedData] = useState(0);
     const [totalScore, setTotalScore] = useState(0);
+    const [leftScore, setLeftScore] = useState(0);
+    const [rightScore, setRightScore] = useState(0);
+    const handleLeftClick = () => {
+        setLeftScore(leftScore + totalScore); // Add middle score to left
+        setTotalScore(0); // Reset middle score
+    };
+
+    const handleRightClick = () => {
+        setRightScore(rightScore + totalScore); // Add middle score to right
+        setTotalScore(0); // Reset middle score
+    };
     return(
         <div className='board'>
              <Header sharedData={sharedData} setSharedData={setSharedData}/>
              <Score totalScore = {totalScore}/>
             <div className='oval'></div>
-            <div className='rectangle-left'>
-                <Score/>
+            <div className='rectangle-left' onClick={handleLeftClick}>
+                <Score totalScore={leftScore}/>
             </div>
-            <div className='rectangle-right'>
-                <Score/>
+            <div className='rectangle-right' onClick={handleRightClick}>
+                <Score totalScore = {rightScore} />
             </div>
             <div className='middle'>
                 <QuizNew sharedData={sharedData} totalScore = {totalScore} setTotalScore = {setTotalScore}/>
