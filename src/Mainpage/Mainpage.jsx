d95d6b5192fed7542c241df5f5e5327298984413
himekatie <useforanything9@gmail.com>
2025-02-03 22:09:10 -0500

    Changed order of cards

--- src/Mainpage/Mainpage.jsx
+++ src/Mainpage/Mainpage.jsx
@@ -4,17 +4,6 @@ import Board from "../Board/Board";
 
 
 function Mainpage(){
-    // useEffect(() => {
-    //     const audio = new Audio('/path-to-your-background-sound.mp3'); // Replace with your sound file
-    //     audio.loop = true; // Makes the sound loop
-    //     audio.play();
-    
-    //     // Cleanup function to stop the sound when the component unmounts
-    //     return () => {
-    //       audio.pause();
-    //       audio.currentTime = 0; // Reset playback to the start
-    //     };
-    //   }, []);
 
       return(
         // Could put form here once code is finished
@@ -23,15 +12,9 @@ function Mainpage(){
         </div>
     
             
-    )
+    );
 
-
-    
-
-
-
-
-}
+};
 
 
 export default Mainpage;
